677e604341a6c5352bf24bd9f6ac6e45a1edcb4e
Kamil Kolodynski <kamilkolodynski@gmail.com>
2015-05-16 20:15:02 +0100

    Added styling to word cloud, removed controller, updated api messages

--- src/app/components/word_cloud/directive.js
+++ src/app/components/word_cloud/directive.js
@@ -1,14 +1,29 @@
 (function () {
     'use strict';
 
-    var wordCloud = angular.module('wordCloud');
+    var wordCloud = angular.module('wordCloud', ['config']);
 
-    wordCloud.directive('cloud', ['CONFIG', '$templateCache', function (CONFIG, $templateCache) {
+    wordCloud.directive('cloud', ['CONFIG', '$templateCache', 'Navigation', '$rootScope', function (CONFIG, $templateCache, Navigation, $rootScope) {
         return {
             restrict: 'E',
             replace: true,
-            controller: 'WordCloudController',
-            template: $templateCache.get(CONFIG.URL.TEMPLATE.WORD_CLOUD)
+            template: $templateCache.get(CONFIG.URL.TEMPLATE.WORD_CLOUD),
+            link: function(scope){
+                scope.$watch('data', function(newValue, oldValue) {
+                    if(newValue === oldValue && !scope.data) {
+                        return;
+                    }
+                    reload(scope);
+                }, true);
+
+                function reload(scope) {
+                    scope.topics = $rootScope.data.topics;
+                }
+
+                scope.selectWord = function(id) {
+                    Navigation.navigate(id);
+                };
+            }
         };
     }]);
 
